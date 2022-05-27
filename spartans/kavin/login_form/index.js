7122a44aa686754eec0381ab6e247e55dbcf4aa1
kavinkumar29 <kavinkumar.v2020eee@sece.ac.in>
2022-05-27 09:19:05 +0530

    [kavin]added javascript test file

---
+++ spartans/kavin/login_form/index.js
@@ -0,0 +1,11 @@
+function login(){
+    var name = document.getElementById("name").value
+    var password = document.getElementById("password").value
+    
+    // login_details={
+    //     name_:name,
+    //     password_:password
+    // }
+    document.getElementById("display").innerHTML=name
+
+}
