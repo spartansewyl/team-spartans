43c6f979168fca03c2ee2441dc41bd763c6cb7c5
kavinkumar29 <kavinkumar.v2020eee@sece.ac.in>
2022-07-16 09:25:45 +0530

    modified the breadkingbad api

--- JavaScript/Kavin/script.js
+++ JavaScript/Kavin/script.js
@@ -6,31 +6,53 @@ function _fetchData() {
         console.log(response);
         response.forEach((item) => {
             console.log(item);
-            let ele1 = document.querySelector(".name");
-            let brk = document.createElement("br");
            
+            let ele1 = document.querySelector(".content");
+            let breakEle = document.createElement("br");
+
+            let img = document.createElement("img");
+            img.setAttribute("src",item.img)
+            img.style.width="150px";
+            img.style.height="200px";
+           img.style.marginLeft="30px"
+            ele1.append(img);
+            ele1.append(breakEle);
 
             let ele2 = document.createElement("label");
             ele2.setAttribute("for", item);
             ele2.textContent = item.name;
             ele2.setAttribute("class", "label");   
             ele1.append(ele2);
-            ele1.append(brk);
+            ele1.append(breakEle);
+          
+            let nickname = document.createElement("label");
+            nickname.setAttribute("for", item);
+            nickname.textContent = `Nickname:${item.nickname}`;
+            nickname.setAttribute("class", "label");  
+            ele1.append(nickname);
+            ele1.append(breakEle);
             
-            
-            let dob = document.createElement("label");
-            dob.setAttribute("for", item);
-            dob.textContent = item.birthday;
-            dob.setAttribute("class", "label");  
-            ele1.append(dob);
-            
-            
-            let img = document.createElement("img");
-            img.setAttribute("src",item.img)
-            img.style.width="50%";
-            img.style.height="50%";
-            ele1.append(img)
-            ele1.append(brk);
+            let occupation = document.createElement("label");
+            occupation.setAttribute("for", item);
+            occupation.textContent = `Occupation:${item.occupation}`;
+            occupation.setAttribute("class", "label");  
+            ele1.append(occupation);
+            ele1.append(breakEle);
+
+            let potrayed = document.createElement("label");
+            potrayed.setAttribute("for", item);
+            potrayed.textContent = `Potrayed:${item.potrayed}`;
+            potrayed.setAttribute("class", "label");  
+            ele1.append(potrayed);
+            ele1.append(breakEle);
+
+            let status = document.createElement("label");
+            status.setAttribute("for", item);
+            status.textContent = `Status:${item.status}`;
+            status.setAttribute("class", "label");  
+            // status.append(status);
+            ele1.append(breakEle);
+
         });
       })
     }
