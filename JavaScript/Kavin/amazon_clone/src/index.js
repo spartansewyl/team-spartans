00a98216498fbf60df5bfb6ce4a24d6f54cf9f6c
kavinkumar29 <kavinkumar.v2020eee@sece.ac.in>
2022-07-20 12:24:34 +0530

    [kavin]-modified checkout page in amazon

--- JavaScript/Kavin/amazon_clone/src/index.js
+++ JavaScript/Kavin/amazon_clone/src/index.js
@@ -9,12 +9,12 @@ import reducer  from "./reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+ 
     
     <StateProvider initialState={initialState} reducer={reducer}>
       <App />
     </StateProvider>
-  </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
--- JavaScript/Kavin/amazon_clone/src/reducer.js
+++ JavaScript/Kavin/amazon_clone/src/reducer.js
@@ -1,3 +1,5 @@
+// const getLocalStoreData = JSON.parse(localStorage.getItem("basketItems")) || [];
+
 export const initialState = {
   basket: [],
   user: null,
@@ -9,15 +11,31 @@ export const gettotal = (basket) =>
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_BASKET":
+      // localStorage.setItem('basketItems', JSON.stringify([...state.basket, action.item]));
       return {
         ...state,
         basket: [...state.basket, action.item],
       };
     case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in basket!`
+        );
+      }
+
       return {
         ...state,
-        basket: state.basket.filter((item) => item.id !== action.id),
+        basket: newBasket,
       };
+    default:
+      return state;
   }
 };
 export default reducer;
